b5a4b5a0c6a9911f2962e9ca3bf4fc249600823c
ShubhamNsoftechnology <129149559+ShubhamNsoftechnology@users.noreply.github.com>
2023-04-14 08:48:37 +0530

    Delete table.sql

--- table.sql
+++
@@ -1,7 +0,0 @@
-Create Table student1(
-    id int Not NULL AUTO_INCREMENT,
-    -- name varchar(255) NOT NULL,
-    student_name varchar(255),
-    student_contact integer,
-    PRIMARY KEY(id)
-);
